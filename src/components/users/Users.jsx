3ea615dfef30005f51e312ec71245fc84ed102d6
lecras@tut.by <128vitrumx>
2021-06-07 13:59:15 +0300

    53

--- src/components/users/Users.jsx
+++ src/components/users/Users.jsx
@@ -1,17 +1,19 @@
 import React from "react";
 import s from './Users.module.css';
-import axios from "axios";
+import * as axios from "axios";
 import userPhoto from "../../assets/images/user-avatar.png"
 
-let Users = (props) => {
-
-    if (props.users.length === 0) {
-        axios.get("https://social-network.samuraijs.com/api/1.0/users").then(resp => props.setUsers(resp.data.items));
+class Users extends React.Component {
+    constructor(props) {
+        super(props);
+        axios.get("https://social-network.samuraijs.com/api/1.0/users").then(resp => this.props.setUsers(resp.data.items));
     }
 
-    return <div>
-        {
-            props.users.map(u => <div key={u.id}>
+
+    render() {
+        return <div>
+            {
+                this.props.users.map(u => <div key={u.id}>
           <span>
               <div className={s.userPhoto}>
                   <img src={u.photos.small != null ? u.photos.small : userPhoto}/>
@@ -19,14 +21,14 @@ let Users = (props) => {
               <div>
                   {u.followed
                       ? <button onClick={() => {
-                          props.unfollow(u.id)
+                          this.props.unfollow(u.id)
                       }}>Unfollow</button>
                       : <button onClick={() => {
-                          props.follow(u.id)
+                          this.props.follow(u.id)
                       }}>Follow</button>}
               </div>
           </span>
-                <span>
+                    <span>
                     <span>
                         <div>{u.name}</div>
                          <div>{u.status}</div>
@@ -36,8 +38,10 @@ let Users = (props) => {
                         <div>{"u.location.country"}</div>
                     </span>
                 </span>
-            </div>)
-        }
-    </div>
+                </div>)
+            }
+        </div>
+    }
 }
+
 export default Users;
--- src/redux/usersReducer.js
+++ src/redux/usersReducer.js
@@ -3,32 +3,7 @@ const UNFOLLOW = 'UNFOLLOW';
 const SET_USERS = 'SET_USERS';
 
 let initialState = {
-    users: [
-        /* {
-             id: 1,
-             photoUrl: 'https://yt3.ggpht.com/a/AATXAJwP-Cll0SICtcx5Yyddhrkcl4RKJZNVtyu9vuQn=s900-c-k-c0x00ffffff-no-rj',
-             followed: false,
-             fullName: "Dmitrii",
-             status: "hello1",
-             location: {city: "Minsk", country: "Belarus"}
-         },*/
-        /*   {
-               id: 2,
-               photoUrl: 'https://th.bing.com/th/id/OIP.5eTzsiVmsszqm8ZoCYf5EwHaH7?pid=ImgDet&rs=1',
-               followed: true,
-               fullName: "Dmitrii",
-               status: "hello2",
-               location: {city: "Minsk", country: "Belarus"}
-           },
-           {
-               id: 3,
-               photoUrl: 'https://th.bing.com/th/id/OIP.5eTzsiVmsszqm8ZoCYf5EwHaH7?pid=ImgDet&rs=1',
-               followed: false,
-               fullName: "Dmitrii",
-               status: "hello3",
-               location: {city: "Minsk", country: "Belarus"}
-           }*/
-    ]
+    users: [ ]
 };
 
 const usersReducer = (state = initialState, action) => {
@@ -56,9 +31,8 @@ const usersReducer = (state = initialState, action) => {
         case SET_USERS: {
             return {
                 ...state,
-                //rewtite doesn't work
-                /* users: [...state.users, action.users]*/
-                users: action.users
+                users: [...state.users, ...action.users]
+
             }
         }
         default:
