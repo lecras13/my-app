17b946ecfca6123887662a3dad99785f5733320c
lecras@tut.by <128vitrumx>
2021-06-16 10:30:30 +0300

    85

--- src/components/profile/profileInfo/ProfileStatusWithHooks.jsx
+++ src/components/profile/profileInfo/ProfileStatusWithHooks.jsx
@@ -1,10 +1,16 @@
-import React, {useState} from "react";
+import React, {useEffect, useState} from "react";
 
 
 const ProfileStatusWithHooks = (props) => {
 
     let [editMode, setEditMode] = useState(false);
     let [status, setStatus] = useState(props.status);
+
+
+    useEffect(() => {
+        setStatus(props.status);
+    }, [props.status]);
+
 
     const activateMode = () => {
         setEditMode(true);
